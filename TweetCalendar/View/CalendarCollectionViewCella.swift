2281e606b409182b4e24bdfbbf601624b13bba27
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-07 23:42:27 +0900

    Add Calendar表示

---
+++ TweetCalendar/View/CalendarCollectionViewCella.swift
@@ -0,0 +1,43 @@
+//
+//  CalendarCollectionViewCella.swift
+//  TweetCalendar
+//
+//  Created by 築山朋紀 on 2018/12/07.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCollectionViewCella: UICollectionViewCell {
+    @IBOutlet private var label: UILabel!
+    
+    func setUp(text: String, at indexPath: IndexPath) {
+        label.text = text
+        
+        //Colorの設定
+        if indexPath.row == 0 {
+            //indexPath.rowが0のときは日曜日
+            label.textColor = .red
+        }else if indexPath.row == 6 || indexPath.row == 13 || indexPath.row == 20 || indexPath.row == 27 || indexPath.row == 34 || indexPath.row == 41 {
+            //土曜日
+            label.textColor = UIColor.blue
+        }else if indexPath.row % 7 == 0 {
+            //Cell番号が7の倍数の時(日曜日)
+            label.textColor = .red
+        }else {
+            label.textColor = .black
+        }
+        
+        if indexPath.row <= 5 {
+            //当月以外の日にちのカラーをグレーに変更する
+            if label.text == "25" || label.text == "26" || label.text == "27" || label.text == "28" || label.text == "29" || label.text == "30" || label.text == "31" {
+                label.textColor = .gray
+            }
+        }else if indexPath.row >= 28 {
+            //当月以外の日にちのカラーをグレーに変更する
+            if label.text == "1" || label.text == "2" || label.text == "3" || label.text == "4" || label.text == "5" || label.text == "6" || label.text == "7" || label.text == "8" || label.text == "9" || label.text == "10" || label.text == "11" || label.text == "12" || label.text == "13" || label.text == "14" {
+                label.textColor = .gray
+            }
+        }
+    }
+}
---
+++ TweetCalendar/View/CalendarViewController.swift
@@ -0,0 +1,121 @@
+//
+//  CalendarViewController.swift
+//  TweetCalendar
+//
+//  Created by 築山朋紀 on 2018/12/07.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    @IBOutlet private var collectionView: UICollectionView!
+    
+    private var calendar = Calendar.current
+    private var currentDate = Date()
+    private var numberOfItems: Int?
+    private var daysPerWeek = 7
+    private var currntMonthOfDates = [Date]()
+    private var selectedDate = Date()
+    private var days = 0
+    private var Array = [String]()
+    
+    private var y = 0
+    private var m = 0
+    private var d = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let date = Date()
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        y = year
+        m = month
+        d = day
+        
+        self.navigationItem.title = "\(year)年\(month)月"
+        
+        firstWeek(year: year, month: month)
+        
+//        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension CalendarViewController: UICollectionViewDataSource {
+    //Cellの総数を返す
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 42
+    }
+    
+    //Cellに値を設定
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CalendarCollectionViewCella
+        cell.backgroundColor = .white
+        cell.setUp(text: Array[indexPath.row], at: indexPath)
+        return cell
+    }
+}
+
+extension CalendarViewController {
+    private func firstWeek(year: Int, month: Int) {
+        guard let august2017 = calendar.date(from: DateComponents(year: year, month: month)) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        dateFormatter.calendar = calendar
+        let yearMonthFomatter = DateFormatter()
+        yearMonthFomatter.dateFormat = "yyyy 年 M 月"
+        yearMonthFomatter.calendar = calendar
+        Array.removeAll()
+        self.navigationItem.title = "\(y)年\(month)月"
+        
+        var components = calendar.dateComponents([.year, .month], from: august2017)
+        components.weekOfMonth = 1  //第1週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        components.weekOfMonth = 2 //第2週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        components.weekOfMonth = 3 //第3週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        components.weekOfMonth = 4 //第4週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        components.weekOfMonth = 5 //第5週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        components.weekOfMonth = 6 //第6週目
+        for weekday in 1...7 {
+            components.weekday = weekday  // 曜日
+            if let date = calendar.date(from: components) {
+                Array.append(dateFormatter.string(from: date))
+            }
+        }
+        collectionView.reloadData()
+    }
+}
